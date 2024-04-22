e8775bb1c3c183538091e5bd8b3da5f54166fb8b
Bruno Pacheco da Silva <bruno.silva@caed.ufjf.br>
2024-04-22 20:53:40 -0300

    gh pages workflow base 7

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 export default defineConfig({
+    base: "/farmacia-front-main/",
     plugins: [
         vue(),
         vueJsx()
